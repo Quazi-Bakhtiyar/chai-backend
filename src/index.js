cec4c0a54f98c31aac78b16c87cdbd592a31c187
Quazi Bakhtiyar Ali <indianmedievelhistory@gmail.com>
2026-07-02 10:10:38 +0530

    s

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
-
+import {app} from './app.js'
 // dotenv.config({
 //     path: './.env'
 // })
 
 connectDB()
+.then(app.listen(process.env.PORT , ()=>{
+    console.log(`Server is running at Port : ${process.env.PORT}`)
+}))
+.catch((err)=>{
+    console.log(`database connection failed : ${err}`)
+})
 
 
 
-/*
 
-;(async()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    } catch (error) {
-        console.error(error)
-        throw error
-    }
-})()
-    */
